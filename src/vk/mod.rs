dac70d6551b0e9534db694b20d88d6cf9dbcee09
Ytawo <milan.sylvain@gmail.com>
2018-06-24 19:05:27 +0200

    Generation script now refresh mod.rs

--- src/vk/mod.rs
+++ src/vk/mod.rs
@@ -1,36 +1,31 @@
 mod utils;
-mod vk_instance;
-mod vk_physical_device;
-
 mod vk_application_info;
-mod vk_instance_creation_info;
-mod vk_queue_family_properties;
-mod vk_queue_capabilities;
 mod vk_extent_3d;
-
+mod vk_instance;
+mod vk_instance_creation_info;
+mod vk_physical_device;
+mod vk_physical_device_features;
+mod vk_physical_device_limits;
+mod vk_physical_device_properties;
+mod vk_physical_device_sparse_properties;
+mod vk_physical_device_type;
+mod vk_queue_family_properties;
+mod vk_queue_flags;
 mod vk_result;
 mod vk_structure_type;
-mod vk_physical_device_type;
-mod vk_physical_device_limits;
-mod vk_physical_device_sparse_properties;
-mod vk_physical_device_properties;
-mod vk_physical_device_features;
-
 
 pub use self::utils::*;
-pub use self::vk_instance::*;
-pub use self::vk_physical_device::*;
-
 pub use self::vk_application_info::*;
-pub use self::vk_instance_creation_info::*;
-pub use self::vk_queue_family_properties::*;
-pub use self::vk_queue_capabilities::*;
 pub use self::vk_extent_3d::*;
-
+pub use self::vk_instance::*;
+pub use self::vk_instance_creation_info::*;
+pub use self::vk_physical_device::*;
+pub use self::vk_physical_device_features::*;
+pub use self::vk_physical_device_limits::*;
+pub use self::vk_physical_device_properties::*;
+pub use self::vk_physical_device_sparse_properties::*;
+pub use self::vk_physical_device_type::*;
+pub use self::vk_queue_family_properties::*;
+pub use self::vk_queue_flags::*;
 pub use self::vk_result::*;
 pub use self::vk_structure_type::*;
-pub use self::vk_physical_device_type::*;
-pub use self::vk_physical_device_limits::*;
-pub use self::vk_physical_device_sparse_properties::*;
-pub use self::vk_physical_device_properties::*;
-pub use self::vk_physical_device_features::*;
