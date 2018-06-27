3d7f9f9c73f36ba8da1648c63efd4f5172f8eed0
Ytawo <milan.sylvain@gmail.com>
2018-06-27 20:05:40 +0200

    Removed gcc build.

--- build.rs
+++ build.rs
@@ -1,20 +1,12 @@
-extern crate gcc;
-
-use std::*;
+use std::env;
 
 fn main() {
-    let mut build = gcc::Build::new();
-    
-    match env::var("VULKAN_SDK") {
+     match env::var("VULKAN_SDK") {
         Ok(path) => {
-            build.include(format!("{}/include", path));
             println!("cargo:rustc-link-search={}/lib", path);
         },
         Err(_) => ()
     };
-    
-    build.file("src/c/vkw.c");
-    build.compile("libvkw.a");
-    
+
     println!("cargo:rustc-link-lib=vulkan");
 }
