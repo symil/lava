c1dfce029cbf44a9622410b24880fd743186ae06
Anovs <milan.sylvain@gmail.com>
2019-02-04 20:25:47 +0100

    Dot.

--- scripts/utils.js
+++ scripts/utils.js
@@ -587,7 +587,7 @@ function stringToFunction(statement, varName, countVarName, arrayVarName, varNam
 
 function documentType(def, precision) {
     const typeName = (def.typeName || def.name).replace(/Flags$/, 'FlagBits') + (def.extension || '').toUpperCase();
-    let str = `/// Wrapper for [${typeName}](https://www.khronos.org/registry/vulkan/specs/1.1-extensions/man/html/${typeName}.html)`;
+    let str = `/// Wrapper for [${typeName}](https://www.khronos.org/registry/vulkan/specs/1.1-extensions/man/html/${typeName}.html).`;
 
     if (precision) {
         str += `\n///\n${precision}`;
